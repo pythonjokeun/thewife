9395a8e68df08aea9e72b60924b911374a37e874
Ichsan Hariadi <sniffles@mail.com>
2018-06-03 10:14:58 +0700

    Bump MFI version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thewife',
-    version='0.1-MFI',
+    version='0.1.1-MFI',
     description='Trading bot that reacts to optimized RSI indicator',
     packages=['thewife'],
     install_requires=[
